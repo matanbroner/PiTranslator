80c3029145ed7cc4d13cf213ffd0d370c83f7d2a
Matan Broner <matanbroner@gmail.com>
2021-12-05 14:51:02 -0600

    update node version

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 # ./setup.sh
 #
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 
