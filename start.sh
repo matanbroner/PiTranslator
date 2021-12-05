e26f8937a2267594aa7d5b0aae1df36ae91f35ef
Matan Broner <matanbroner@gmail.com>
2021-12-05 15:04:02 -0600

    sudo

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export GOOGLE_APPLICATION_CREDENTIALS="$SCRIPT_DIR/google_api_keys.json"
 
 cd $SCRIPT_DIR/src/speech
-node index.js
+sudo node index.js
