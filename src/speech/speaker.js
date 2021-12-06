8fbdd28575633bf4f9cf3c3a2edc8fb254c6a302
Matan Broner <matanbroner@gmail.com>
2021-12-05 18:19:46 -0600

    change player

--- src/speech/speaker.js
+++ src/speech/speaker.js
@@ -1,7 +1,8 @@
 // Import the Google Cloud client library
 const textToSpeech = require("@google-cloud/text-to-speech");
-const Sound = require("node-aplay");
+const mpg = require('mpg123');
 const exec = require("child_process").exec;
+const { randomString } = require('./util')
 
 // Import other required libraries
 const fs = require("fs");
@@ -25,28 +26,26 @@ async function speakTranslatedText(text) {
   const [response] = await client.synthesizeSpeech(request);
   // Write the binary audio content to a local file
   const writeFile = util.promisify(fs.writeFile);
-  await writeFile("output.mp3", response.audioContent, "binary");
+  const filename = `${randomString(10)}.mp3`;
+  await writeFile(filename, response.audioContent, "binary");
   console.log("Audio content written to file: output.mp3");
 
   // if is MacOS
   if (process.platform === "darwin") {
     exec("afplay output.mp3", () => {
       // remove file when done
-      fs.unlink("output.mp3", () => {
+      fs.unlink(filename, () => {
         console.log("File removed");
       });
     });
   } else {
-    var music = new Sound("output.mp3");
-    music.play();
-
-    // you can also listen for various callbacks:
-    music.on("complete", function () {
-      // remove the file when done
-      fs.unlink("output.mp3", function (err) {
-        console.log("File deleted!");
-      });
-    });
+    const player = new mpg.MpgPlayer();
+    player.play(filename);
+    player.on('end', function(){
+        fs.unlink(filename, function (err) {
+            console.log("File deleted!");
+          });
+    })
   }
 }
 
